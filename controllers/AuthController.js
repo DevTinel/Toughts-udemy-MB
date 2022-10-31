09c6721d3ee232bac9fa3b8267422399dab4182a
DevTinel <davidaugustolol36@gmail.com>
2022-10-30 21:00:04 -0300

    criando usuario, criando seção do usuario, validacoes do formulario,flash messages

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,8 +1,47 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
 module.exports = class AuthController {
   static login(req, res) {
     res.render('auth/login');
   }
   static register(req, res) {
     res.render('auth/register');
+  }
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+    //verificando se as senhas conferem
+    if (password != confirmpassword) {
+      req.flash('message', 'As senhas não conferem!');
+      res.render('auth/register');
+
+      return;
+    }
+    //checar se o email ja existe
+    const checkIfUserExists = await User.findOne({ where: { email: email } });
+    if (checkIfUserExists) {
+      req.flash('message', 'O email ja está cadastrado!');
+      res.render('auth/register');
+    }
+    //criar senha
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+    try {
+      const createdUser = await User.create(user);
+      //logar o usuario assim que for cadastrado
+      req.session.userid = createdUser.id;
+      req.session.save(() => {
+        res.redirect('/');
+      });
+
+      req.flash('message', 'Cadastro realizao com sucesso');
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
